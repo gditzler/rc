21926a56c66b4ac0ec202fe96c89645d6978e94a
Gregory Ditzler <gregory.ditzler@gmail.com>
2023-12-07 08:50:19 -0500

    corrected init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,2 @@
--- define your colorscheme here
-local colorscheme = 'nord'
-
-local is_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not is_ok then
-    vim.notify('colorscheme ' .. colorscheme .. ' not found!')
-    return
-end
-
+require('options')
+require('plugins')
